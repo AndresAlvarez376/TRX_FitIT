68fec1319c4db2f1fab77871311f8564962efc2f
AndresAlvarez376 <33386328+AndresAlvarez376@users.noreply.github.com>
2017-11-16 21:10:38 -0500

    Update UserDAO.java

--- Trx_FitITWEB/src/dataAccessObject/UserDAO.java
+++ Trx_FitITWEB/src/dataAccessObject/UserDAO.java
@@ -1,5 +1,25 @@
 package dataAccessObject;
 
-public class UserDAO {
+import modelo.GetConnection;
 
+public class UserDAO {
+    public boolean validateUser(String uName, String pass){
+      String sql="select * from user where uname =? and pass =?";
+      
+      GetConnection gc= new GetConnection();
+      
+      try{
+      gc.ps = GetConnection.getMySQLConnection().preparedStatement(sql);
+      gc.ps.setString(1, uName);
+      gc.ps.setString(2, pass);
+      
+      gc.ps = gc.ps.executeQuery();
+      
+      return gc.ps.next();
+      }catch (SQLException e){
+        e.printStackTrace();
+      }
+      
+      return false;
+    }
 }
